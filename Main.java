01f0d91a0fa284f84b7b558477f5ce0c6e66c1af
PRASANTH G <prasanth287471@gmail.com>
2021-03-25 16:58:17 +0000

    code added

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.io.*; 
+import java.util.*;
+class Main{
+    
+    
+    public static void main (String[] args) {
+        
+      int[] x = new int[4];
+      Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value to insert  "+x.length);
+      for(int i=0;i<x.length;i++)
+      {
+          x[i]= sc.nextInt();
+            System.out.println();
+          
+      }
+        System.out.println("\n values  are ");
+      for(int j=0;j<x.length;j++)
+      {
+         System.out.println(x[j]+" ");
+      }
+    }
 }
